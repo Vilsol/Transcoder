4ad6811e0eac2cc3342c7d411e5a077352c01fb9
Vilsol <vilsol2000@gmail.com>
2017-10-05 07:20:34 +0300

    Fix TG notification file name, fix transcode failures

--- transcoder.py
+++ transcoder.py
@@ -40,12 +40,11 @@ def transcode(file, pbar, desc, frames):
 		'(.+)'
 	])
 
-	basename = os.path.basename(file + ".processed")
 	original = os.path.getsize(file)
 	finished = True
 
 	currentMessage = None
-	update_message(prepare_message(basename, original, 0, 0))
+	update_message(prepare_message(os.path.basename(file), original, 0, 0))
 
 	counter = 0
 	while True:
@@ -94,7 +93,7 @@ def transcode(file, pbar, desc, frames):
 					pbar.set_description(desc + " ({})".format(convert_size(new_size)))
 
 					if counter % 10 == 0:
-						update_message(prepare_message(basename, original, new_size, (previous_frame / frames) * 100))
+						update_message(prepare_message(os.path.basename(file), original, new_size, (previous_frame / frames) * 100))
 
 					counter = counter + 1
 
@@ -111,20 +110,22 @@ def transcode(file, pbar, desc, frames):
 		converted = os.path.getsize(file + '.new.mkv')
 
 		directory = os.path.dirname(file)
-		with open(directory + "/." + basename, 'w') as f:
+		with open(directory + "/." + os.path.basename(file + ".processed"), 'w') as f:
 			if original < converted:
 				f.write(str(original))
 				os.remove(file + '.new.mkv')
-			else:
+			elif converted > 1000000:
 				f.write(str(converted))
 				os.remove(file)
 				os.rename(file + '.new.mkv', file)
+			else:
+				finished = False
 
 		return original, converted, finished
 
 	print("Stopping...")
 
-	update_message(prepare_stopping_message(basename, original, new_size, (previous_frame / frames) * 100))
+	update_message(prepare_stopping_message(os.path.basename(file), original, new_size, (previous_frame / frames) * 100))
 
 	os.remove(file + '.new.mkv')
 
@@ -340,16 +341,20 @@ def search(path, name, depth=0, prefix='', last=True):
 					oldsize = convert_size(result[0])
 					newsize = convert_size(result[1])
 
-					if result[2]:
-						if result[1] > result[0]:
-							print('{} -> {} ({}%) (kept old)'.format(oldsize, newsize, diff))
-							update_message('*{}*\n*Size:* {} --> {} ({}%)\n*Status:* Kept old'.format(name, oldsize, newsize, diff))
+					if result[3]:
+						if result[2]:
+							if result[1] > result[0]:
+								print('{} -> {} ({}%) (kept old)'.format(oldsize, newsize, diff))
+								update_message('*{}*\n*Size:* {} --> {} ({}%)\n*Status:* Kept old'.format(name, oldsize, newsize, diff))
+							else:
+								print('{} -> {} ({}%)'.format(oldsize, newsize, diff))
+								update_message('*{}*\n*Size:* {} --> {} ({}%)\n*Status:* Replaced with new'.format(name, oldsize, newsize, diff))
 						else:
-							print('{} -> {} ({}%)'.format(oldsize, newsize, diff))
-							update_message('*{}*\n*Size:* {} --> {} ({}%)\n*Status:* Replaced with new'.format(name, oldsize, newsize, diff))
+							print('{} -> {} ({}%) (kept old)'.format(oldsize, newsize, diff))
+							update_message('*{}*\n*Size:* {} --> Gave up at {} ({}%)\n*Status:* Kept old'.format(name, oldsize, newsize, diff))
 					else:
 						print('{} -> {} ({}%) (kept old)'.format(oldsize, newsize, diff))
-						update_message('*{}*\n*Size:* {} --> Gave up at {} ({}%)\n*Status:* Kept old'.format(name, oldsize, newsize, diff))
+						update_message('*{}*\n*Size:* {} --> Failed at {} ({}%)\n*Status:* Kept old'.format(name, oldsize, newsize, diff))
 				elif result[0] == 0:
 					print('failed')
 
